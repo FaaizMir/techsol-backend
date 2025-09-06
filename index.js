7d413629469677e8002214b040e7d41363c0eeff
FaaizMir <faaizmir123@gmail.com>
2025-09-06 15:41:23 -0700

    tt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const sequelize = require('./models/index')
 
 const app = express()
-app.use(cors())
+app.use(cors({ origin: '*' })); // tighten origin in prod
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)

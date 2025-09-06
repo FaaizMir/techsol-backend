4f9245161cb3af74adac5977452491f6abacd2fa
FaaizMir <faaizmir123@gmail.com>
2025-09-06 14:48:18 -0700

    yyyyyyyyy

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,10 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+
+
+
+
 const router = express.Router()
 
 // Signup

35516f76fb3983c88df4dfb0bfa99941a971fd9d
FaaizMir <faaizmir123@gmail.com>
2025-09-07 15:34:42 -0700

    8th sept

--- models/index.js
+++ models/index.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // Database configuration from environment with safe defaults
 const dbName = process.env.DB_NAME || 'techsolutions'
-const dbUser = process.env.DB_USER || 'root'
-const dbPass = process.env.DB_PASS || ''
-const dbHost = process.env.DB_HOST || 'localhost'
-const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306
-const dbDialect = process.env.DB_DIALECT || 'mysql'
+const dbUser = process.env.USERNAME || 'root'
+const dbPass = process.env.PASSWORD || ''
+const dbHost = process.env.HOST || 'localhost'
+const dbPort = process.env.PORT ? parseInt(process.env.PORT, 10) : 3306
+const dbDialect =  'mysql'
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
